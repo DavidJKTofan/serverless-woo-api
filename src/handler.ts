8c58a0e5da9ea67ca4aa5eb4b3bb249cc889b6cf
DavidJKTofan <dtofan@learning.eoi.es>
2022-07-26 08:38:34 +0200

    Update handler.ts

--- src/handler.ts
+++ src/handler.ts
@@ -12,4 +12,7 @@ router
   .get('/api/resources/:main_cat1', Main_Cat1)
   .get('*', () => new Response("Not found", { status: 404 }))
 
+console.log(Main_Cat1)
+console.log({ Main_Cat1 })
+
 export const handleRequest = request => router.handle(request)
